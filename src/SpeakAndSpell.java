8d8ad5a3a2f8e8738d90e2238f9de84155a58b7a
armandoM55 <league@Mac-mini-3.local>
2016-05-11 14:58:17 -0700

    cuel
    star wars

---
+++ src/SpeakAndSpell.java
@@ -0,0 +1,45 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Teacher's Note: 
+ * Have the kids play with the Speak & Spell. 
+ * The first Speak & Spell was introduced at the summer Consumer Electronics Show in June 1978, making it 
+ * one of the earliest handheld electronic devices with a visual display to use interchangeable game cartridges. 
+ * Discuss with students how you would make this program. 
+ * Allow them to code it themselves, or use this recipe.
+ **/
+
+public class SpeakAndSpell {
+
+	public static void main(String[] args) {
+		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+speak("Spell mandlebrot");
+		// 2. Catch the user's answer in a String
+String answer=JOptionPane.showInputDialog(null);
+		// 3. If the user spelled the word correctly, speak "correct"
+if (answer.equals("mandlebrot")) {
+	speak("correct");
+}
+		// 4. Otherwise say "wrong"
+else {
+	speak("wrong");
+}
+		// 5. repeat the process for other words
+		speak("Spell Ryon");
+		String answer1=JOptionPane.showInputDialog(null);
+		if (answer1.equalsIgnoreCase("Ryon")) {
+			speak("correct");
+		} else {
+			speak("Wrong");
+		}
+	}
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/Validation.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Validation {
+	public static void main(String[] args) {
+
+		// 1. Watch the first 2 minutes of this movie: http://vimeo.com/2485018
+		int randomNumber = new Random().nextInt(5);
+		// 2. On paper, write all the numbers that get printed when you run this class
+		System.out.println(randomNumber);
+
+		// 3. Use the randomNumber to give the user a random compliment.
+if (randomNumber==(0)) {
+	System.out.println("You is kind");
+}
+if (randomNumber==(1)) {
+	System.out.println("You is smart");
+}
+if (randomNumber==(2)) {
+	System.out.println("You is loyal");
+}
+if (randomNumber==(3)) {
+	System.out.println("Your meemes are pretty dank");
+}
+if (randomNumber==(4)) {
+	System.out.println("ur ");
+}
+		// 4. Repeat all the code above 10 times
+		
+		// 5. Find someone to test out your program. They will like it :)
+	}
+}
+
